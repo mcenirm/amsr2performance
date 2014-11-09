4188de68e4ebac111399a332d4b1f92d1282fbc7
Michael McEniry (UAH) <mcenirm@uah.edu>
2014-11-09 12:03:47 -0600

    i7z does not work in virtualbox

--- provision.sh
+++ provision.sh
@@ -9,9 +9,6 @@ exec > >(tee -a "${plog}") 2> >(tee -a "${plog}" >&2)
 
 sudo yum install -y time libgfortran libjpeg-turbo perl
 
-sudo yum install -y epel-release
-sudo yum install -y i7z sysbench
-
 sudo groupadd -g 3040 lancedev
 sudo groupadd -g 3043 lanceops
 sudo useradd -u 4106 amsr-e
@@ -20,19 +17,9 @@ sudo tar x -C / -f /vagrant/amsr_toolkits-20141107.tgz
 
 tar xf /vagrant/test.tar.bz2
 
-(cd "${vlog}" ; sudo i7z --nogui --write a & echo $! > i7z.pid)
-sleep 30
-sysbench --test=cpu --cpu-max-prime=20000 run
-sleep 30
-
-cd rainocean 
-bash -x runLance.sh
-
-sleep 30
-sudo kill $(< "${vlog}/i7z.pid")
+cd rainocean && bash -x runLance.sh
 
 cp -p out/rainocean/2014.10.22/AMSR_2_L2_RainOcean_R00_201410222127_A.qa "${vlog}"
 cat /proc/cpuinfo > "${vlog}/cpuinfo.txt"
 cat /proc/meminfo > "${vlog}/meminfo.txt"
-hostname > "${vlog}/hostname.txt"
 
